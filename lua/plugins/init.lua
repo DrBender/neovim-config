ec7ed1730d8dc1df733ccd2deff6c9d8b775578e
DrBender <fedotova.sonya@gmail.com>
2025-11-05 10:53:44 +0300

    Fix old directory names

---
+++ lua/plugins/init.lua
@@ -0,0 +1,21 @@
+return {
+    { import = "plugins.gui.bufferline" },
+    { import = "plugins.gui.themes" },
+    { import = "plugins.gui.neo-tree" },
+    { import = "plugins.gui.snacks" },
+    { import = "plugins.gui.lualine" },
+    { import = "plugins.gui.telescope" },
+    { import = "plugins.gui.which-key" },
+
+    { import = "plugins.lsp.treesitter" },
+    -- { import = "plugins.lsp.none-ls" },
+    { import = "plugins.lsp.linters" },
+
+    { import = "plugins.lsp.formatters" },
+
+    { import = "plugins.lsp.mason" },
+
+    { import = "plugins.code.simple" },
+    { import = "plugins.code.completion" },
+    -- {import = "plugins.lsp.lua_ls_conf"},
+}
